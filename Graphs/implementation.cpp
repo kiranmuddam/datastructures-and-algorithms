58f001527f6a105f258f7f39267a20834730caf7
Kiran Babu Muddam <kiraniiitn@gmail.com>
2020-08-28 13:09:15 +0530

    graphs

---
+++ Graphs/implementation.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+using namespace std;
+
+class Graph {
+private:
+    int vertices;
+    vector<list<int> > array;
+    vector<bool> visited;
+public:
+    Graph(int v) {
+        array.resize(v);
+        vertices = v;
+        visited.resize(v, false);
+    }
+
+    void addEdge(int source, int destination) {
+         if (source < vertices && destination < vertices)
+            array[source].push_front(destination);
+    }
+
+    void printGraph() {
+        cout << "Adjacency List of Directed Graph:" <<endl;
+        cout << endl;
+        list<int>::iterator temp;
+        for(int i=0; i<vertices; i++) {
+            cout << "|" << i << "| => ";
+            temp = (array[i]).begin();
+
+            while(temp != array[i].end()) {
+                cout <<  "[" <<*temp << "] -> ";
+                temp++;
+            }
+            cout <<"NULL"<<endl;
+        }
+    }
+
+    void dfs(int at) {
+        if (visited[at]) {
+            return;
+        }
+        cout << at << ' ';
+        visited[at] = true;
+        list<int>::iterator it;
+
+        for (it = array[at].begin(); it != array[at].end(); it++) {
+            dfs(*it);
+        }
+    }
+
+    void dfsTraversal() {
+        cout << "DFS Traversal" << endl;
+        for (int i = 0; i < vertices; i++) {
+            dfs(i);
+        }
+    }
+};
+
+int main() {
+    Graph g(7);
+    g.addEdge(1, 3);
+    g.addEdge(1, 2);
+    g.addEdge(2, 5);
+    g.addEdge(2, 4);
+    cout << endl;
+    g.printGraph();
+    g.dfsTraversal();
+    cout << endl;
+    return 0;
+}
